31306d84dceb96d7db762267443e86a697d4d93b
Damian Rogoz <d.rogoz@o2.pl>
2020-04-12 18:24:05 +0200

    Add files

---
+++ src/Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    String name;
+    int age;
+    boolean barks;
+
+    Dog(String n, int a, boolean b) {
+        name = n;
+        age = a;
+        barks = b;
+    }
+
+        void showInfo() {
+            System.out.println("Imię: " + name + "Wiek: " + age + "Czy szczeka często: " + barks);
+        }
+
+
+    }
+
+
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Tofik ", 5 , true);
+        Dog dog2 = new Dog("Bruno ", 10 , true );
+
+        dog1.showInfo();
+        dog2.showInfo();
+
+    }
+}
