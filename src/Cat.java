049b45288f41840d4dbfa87088679232a96ce89b
Damian Rogoz <d.rogoz@o2.pl>
2020-04-13 19:40:11 +0200

    Changes in files

---
+++ src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    String name;
+    int age;
+    boolean barks;
+
+    void showInfo() {
+        System.out.println("Imię: " + name + "Wiek: " + age + "Czy szczeka często: " + barks);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Dog dog1 = new Dog("Tofik ", 5 , true);
-        Dog dog2 = new Dog("Bruno ", 10 , true );
+        Dog dog1 = new Dog("Tofik ", 4 , true);
+        Dog dog2 = new Dog("Bruno ", 7 , true );
 
         dog1.showInfo();
         dog2.showInfo();
 
+        Cat cat1 = new Cat();
+        cat1.name = "Salem ";
+        cat1.age = 2;
+        cat1.barks = false;
+
+        cat1.showInfo();
     }
 }
